7fb297f89961bc30ba5249e3e8303bd125102fc2
Ryan Tilleman <apple@MacBook-Pro.local>
2020-10-30 11:01:42 +0100

    Nieuwkomer opdracht

---
+++ nieuwkomer.py
@@ -0,0 +1,303 @@
+
+
+print("Dit is mijn eind opdracht over de nieuwkomer!")
+
+print("Ik heb gekozen om mijn verhaal van een nieuwkomer over te brengen met 21 multiple choise vragen.")
+
+print("Dit verhaal is gebaseerd op een vriend van mij, hij wilt graag niet bij zijn eigen naam genoemd worden daarom noem ik hem moo.")
+
+print("Bij deze multiple choise vragen is het belangrijk dat je bij het antwoorden van de vragen het precies overneemt, dus als je voor b kiest, je ook echt b neer zet en niet B.")
+
+print("Let goed op de hoofdletters en leestekens bij het beantwoorden van de vragen.")
+
+print("Dan wens ik u bij deze veel plezier bij het maken van deze opdracht, en ik hoop dat ik u een goede indruk ga kunnen geven over hoe zijn leven als vluchteling is.")
+
+print("Je geeft antwoord door a, b of c als antwoord te geven, of het antwoord exact over te nemen.")
+
+print("Succes! :D")
+
+print("-----------------------------------------------------------------------------------------------------------------------")
+
+
+
+
+
+
+score = 0
+
+# QUESTION 1
+answer1 = input("Waar komt moo vandaan? \n a. Syrië \n b. Irak \n c. Afghanistan \n : ")
+if answer1 == "a" or answer1 == "Syrië":
+	score += 1
+	print("Correct! Het juiste antwoord is inderdaad Syrië. hij was hier geboren in de plaats Aleppo.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Syrië. Hij was daar geboren in het plaatsje Aleppo.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 2
+answer1 = input("hoe lang heeft hij in Aleppo gewoond? \n a. 1 jaar \n b. 3 jaar \n c. Tot hij vluchtte uit zijn land \n Answer: ")
+if answer1 == "a" or answer1 == "1 jaar":
+	score += 1
+	print("Correct! Hij heeft een jaar samen gewoond met zijn ouders. na een jaar gingen zijn ouders uit elkaar waardoor hij uit Aleppo ging en kwam wonen bij zijn opa en oma, dit kwam vanwege de lage inkomen die zijn moeder had.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Hij heeft maar een jaartje in Aleppo gewoond. na een jaar gingen zijn ouders uit elkaar waardoor hij uit Aleppo ging en kwam wonen bij zijn opa en oma, dit kwam vanwege de lage inkomen die zijn moeder had.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 3
+answer1 = input("wanneer zag hij zijn moeder en vader nog? \n a. niet \n b. 1 keer in het jaar \n c. een paar dagen in de week \n Answer: ")
+if answer1 == "c" or answer1 == "een paar dagen in de week":
+	score += 1
+	print("Correct! Hij ging elk weekend naar zijn moeder toe, zijn vader was veel aan de kant van de stad waar hij met zijn opa en oma woonde, dus die kwam regelmatig langs.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is een paar dagen in de week. Hij ging elk weekend naar zijn moeder toe, zijn vader was veel aan de kant van de stad waar hij met zijn opa en oma woonde, dus die kwam regelmatig langs.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 4
+answer1 = input("Hoelang heeft hij bij zijn opa en oma gewoond? \n a. 3 jaar \n b. 6 jaar \n c. Tot het moment dat hij naar Turkije was gevlucht \n Answer: ")
+if answer1 == "c" or answer1 == "Tot het moment dat hij naar Turkije was gevlucht":
+	score += 1
+	print("Correct! Hij is blijven wonen bij zijn opa en oma tot hij naar Turkije was gevlucht. Hij heeft er in totaal 9 jaar gewoond.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Tot het moment dat hij naar Turkije was gevlucht. Hij heeft er in totaal 10 jaar gewoond. ")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 5
+answer1 = input("Uiteindelijk is hij gevlucht uit Syrië, maar waarom? \n a. Voor Schulden \n b. Oorlog \n c. Familie ruzie \n Answer: ")
+if answer1 == "b" or answer1 == "Oorlog":
+	score += 1
+	print("Correct! Hij is uit Syrië gevlucht vanwege de oorlog die daar bezig was.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Oorlog. Hij is uit Syrië gevlucht omdat er oorlog was.")
+	print("Score: ", score)
+	print("\n")
+
+
+
+# QUESTION 6
+answer1 = input("Met wie is hij naar Turkije gevlucht? \n a. zijn vader en moeder \n b. zijn moeder \n c. zijn ouders en opa en oma \n Answer: ")
+if answer1 == "c" or answer1 == "zijn ouders en opa en oma":
+	score += 1
+	print("Correct! Hij is naar Turkije gegaan samen met zijn ouders en opa en oma.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is zijn ouders en opa en oma. Hij is naar Turkije gegaan samen met zijn ouders en opa en oma.")
+	print("Score: ", score)
+	print("\n")
+print("Bij deze wil ik graag even wat uitleggen. Hij is niet alleen gevlucht voor de oorlog, hij is ook naar Turkije gegaan omdat zijn oma ziek was geworden, en ze had medicatie nodig en ze konden in Syrië geen medicatie vinden voor die ziekte.")
+
+# QUESTION 7
+answer1 = input("Hierna is hij verhuisd naar Staumpul, maar voor hoelang? \n a. 0.5 jaar \n b. 1 jaar \n c. 1.5 jaar \n Answer: ")
+if answer1 == "a" or answer1 == "0.5 jaar":
+	score += 1
+	print("Correct! Hij heeft hier een half jaartje gewoond en is daarna weer verhuisd.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is 0.5 jaar. Hij heeft Hier een half jaartje gewoond en is daarna weer verhuisd.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 8
+answer1 = input("uiteindelijk is hij alweer verhuisd, hij verhuisde naar Izmir. hoelang heeft hij daar gewoond? \n a. 1.5 jaar  \n b. 2 jaar \n c. 2.5 jaar \n Answer: ")
+if answer1 == "a" or answer1 == "1.5 jaar":
+	score += 1
+	print("Correct! Hij heeft hier ander half jaar gewoond.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is 1.5 jaar. hij heeft daar 1.5 jaar gewoond")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 9
+answer1 = input("Ondertussen dat moo in Izmir woonde was zijn moeder weg gegaan, maar naar waar? \n a. terug naar Syrië \n b. Verhuisd naar ergens anders in Turkije \n c. Met een nep paspoort naar Nederland gegaan \n Answer: ")
+if answer1 == "c" or answer1 == "Met een nep paspoort naar Nederland gegaan":
+	score += 1
+	print("Correct! Zij is naar Nederland gegaan met een nep paspoort.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Met een nep paspoort naar Nederland gegaan. moo heeft anderhalf jaar bij zijn opa en oma en vader gewoond.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 10
+answer1 = input("Waarom ging moo niet meteen mee naar Nederland?? \n a. hij wou dat niet \n b. hij moest akkoord hebben om naar Nederland te komen \n c. hij vond het leuk in Turkije  \n Answer: ")
+if answer1 == "b" or answer1 == "hij moest akkoord hebben om naar Nederland te komen":
+	score += 1
+	print("Correct! Anders zou hij illegaal in Nederland zijn, dus ze moesten wachten op akkoord om hem naar Nederland te halen.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is hij moest akkoord hebben om naar Nederland te komen. Anders zou hij hier illegaal zijn.")
+	print("Score: ", score)
+	print("\n")
+
+
+# QUESTION 11
+answer1 = input("Na die anderhalf jaar gewoon te hebben in Izmir kwam zijn moeder terug naar Izmir maar met welk nieuws? \n a. moo en zijn broer mochten mee naar Nederland. \n b. Ze is uit het land gezet \n c. het verzoek was geweigerd. \n Answer: ")
+if answer1 == "a" or answer1 == "moo en zijn broer mochten mee naar Nederland":
+	score += 1
+	print("Correct! Het verzoek was geaccepteerd, moo en zijn broer en moeder mochten naar Nederland toe komen.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is moo en zijn broer mochten mee naar Nederland. Het verzoek was geaccepteerd, ze mogen naar Nederland.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 12
+answer1 = input("Op welke manier zijn ze naar Nederland gekomen? \n a. auto \n b. vliegtuig \n c. andere manier \n Answer: ")
+if answer1 == "b" or answer1 == "vliegtuig":
+	score += 1
+	print("Correct! ze zijn met het vliegtuig vanuit Turkije naar Nederland gevlogen.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is vliegtuig. ze zijn met het vliegtuig vanuit Turkije naar Nederland gevlogen. ")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 13
+answer1 = input("eenmaal aangekomen in Zaanstad ging hij wonen in? \n a. Wormerveer \n b. Assendelft \n c. Krommenie \n Answer: ")
+if answer1 == "c" or answer1 == "Krommenie":
+	score += 1
+	print("Correct! Hij is in Krommenie komen wonen waar ik ook woon.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Krommenie.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 14
+answer1 = input("toen moo in Nederland kwam sprak hij ... Nederlands? \n a. goed \n b. amper \n c. geen \n Answer: ")
+if answer1 == "c" or answer1 == "geen":
+	score += 1
+	print("Correct! Hij sprak nog geen Nederlands")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is geen. Hij sprak nog geen Nederlands.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 15
+answer1 = input("Omdat moo nog geen Nederlands sprak, moest hij dan naar een speciale school of begon hij meteen op het voortgezet en dan Engels sprekend? \n a. speciale school \n b. voortgezet onderwijs \n c. prive les \n Answer: ")
+if answer1 == "a" or answer1 == "speciale school":
+	score += 1
+	print("Correct! hij begon op een speciale school, hier leerde hij de basis van onze taal en cultuur.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is speciale school. hij begon op een speciale school, hier leerde hij de basis van onze taal en cultuur.")
+	print("Score: ", score)
+	print("\n")
+
+
+# QUESTION 16
+answer1 = input("maar omdat hij natuurlijk ook niemand had hier, en de taal slecht sprak ging hij voornamelijk naar? \n a. de bibliotheek \n b. het park \n c. Thuis \n Answer: ")
+if answer1 == "b" or answer1 == "park":
+	score += 1
+	print("Correct! Hij trok veel naar het park toe, en kwam daar jammer genoeg in contact met allemaal foute gasten.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is het park. Hij trok veel naar het park toe, en kwam daar jammer genoeg in contact met allemaal foute gasten.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 17
+answer1 = input("door het veel in het park zat, en naar die speciale school ging, leerde hij onze taal wel snel. Naar welke school ging hij? \n a. Altra (praktijk) \n b. Trias VMBO (VMBO) \n c. Bertrand Russel College (havo/vwo) \n Answer: ")
+if answer1 == "b" or answer1 == "Trias VMBO (VMBO)":
+	score += 1
+	print("Correct! Hij was begonnen op het niveau TL ")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Trias VMBO. Hij begon hier met het niveau TL.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 18
+answer1 = input("zijn vader gaat binnenkort ook naar Nederland komen, maar waar gaat hij wonen? \n a. Rotterdam \n b. Amsterdam \n c. Zaandam \n Answer: ")
+if answer1 == "a" or answer1 == "Rotterdam":
+	score += 1
+	print("Correct! Zo ver het er tot nu toe uit ziet gaat hij waarschijnlijk in Rotterdam wonen.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Rotterdam.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 19
+answer1 = input("Waarom specifiek Rotterdam? \n a. mooie stad \n b. ligt gunstig \n c. er woonde al familie \n Answer: ")
+if answer1 == "c" or answer1 == "er woonde al familie":
+	score += 1
+	print("Correct! hij heeft er al neefjes en andere familie leden wonen, dus wilt er zelf ook gaan wonen. ")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Rotterdam. hij heeft er al neefjes en andere familie leden wonen, dus wilt er zelf ook gaan wonen.")
+	print("Score: ", score)
+	print("\n")
+
+# QUESTION 20
+answer1 = input("Hoe oud is moo nu ondertussen? \n a. 16 \n b. 17 \n c. 18 \n Answer: ")
+if answer1 == "a" or answer1 == "16":
+	score += 1
+	print("Correct! Hij is nu 16 jaar")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is 16 jaar.")
+	print("Score: ", score)
+	print("\n")
+
+
+# QUESTION 21
+answer1 = input("spreekt hij ondertussen vloeiend de taal, en is hij in staat om zelfstandig al zijn dingen te kunnen fixen? \n a. ja, zijn Nederlands is nu goed. \n b. zijn Nederlands is prima \n c. zijn Nederlands is nog matig \n Answer: ")
+if answer1 == "a" or answer1 == "Ja zijn Nederlands is nu goed":
+	score += 1
+	print("Correct! Ja zijn Nederlands is nu goed, hij kan alles bijhouden op school en werkt ondertussen ook veel.")
+	print("Score: ",score)
+	print("\n")
+else:
+	print("Dit is fout! Het antwoord is Ja zijn Nederlands is nu goed. hij kan alles bijhouden op school en werkt ondertussen ook veel.")
+	print("Score: ", score)
+	print("\n")
+
+print("-----------------------------------------------------------------------------------------------------------------------")
+
+print("Dit is mijn eind opdracht over de nieuwkomer!")
+
+print("Ik hoop dat ik jullie hier een redelijk beeld mee heb kunnen geven hoe moo hier in Nederland is gekomen!")
+
+print("Ryan Tilleman")
+
+print("SD1DB")
+
+
+
+
+
+
+
+
+
